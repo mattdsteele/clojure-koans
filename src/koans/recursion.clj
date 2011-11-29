c3e5a448d745d30b7b8d2d5b7a59f4b44041747f
Matt Steele <orphum@gmail.com>
2011-11-28 20:35:09 -0600

    up to a recursive-reverse problem

--- src/koans/recursion.clj
+++ src/koans/recursion.clj
@@ -1,17 +1,17 @@
 (defn is-even? [n]
   (if (= n 0)
-      __
-      (___ (is-even? (dec n)))))
+      true
+      (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-        __
+        acc
         (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  '(1))
 
 (defn factorial [n]
   __)
