0a03f063e33d6a38bda6cf6f787e173e7d099ce4
Matt Steele <orphum@gmail.com>
2011-12-13 07:26:10 -0600

    more refs

--- src/koans/refs.clj
+++ src/koans/refs.clj
@@ -22,12 +22,12 @@
           @the-world))
 
   "Don't forget to do your work in a transaction!"
-  (= 0 (do __
+  (= 0 (do (dosync (ref-set the-world 0))
          @the-world))
 
   "Functions passed to alter may depend on the data in the ref"
   (= 20 (do
-          (dosync (alter the-world ___))))
+          (dosync (alter the-world (fn [x] (+ 20 x))))))
 
   "Two worlds are better than one"
   (= ["Real Jerry" "Bizarro Jerry"]
